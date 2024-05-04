15ff2fc88084b5c821b7b07a3aab23c8d6211e8e
tbale28 <tbale28@wgu.edu>
2024-05-04 14:47:03 -0500

    Build Single Patient scene and add link to home Add Link to single Patient page from main

--- src/main/java/org/example/heart_disease_detector/Controllers/mainController.java
+++ src/main/java/org/example/heart_disease_detector/Controllers/mainController.java
@@ -1,8 +1,17 @@
 package org.example.heart_disease_detector.Controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+import org.example.heart_disease_detector.HeartDiseaseApplication;
+
+import java.io.IOException;
 
 public class mainController {
     @FXML
@@ -10,10 +19,24 @@ public class mainController {
     private Button singlePatient_btn;
     private Button multiPatient_btn;
     private Button checkForUpdate_btn;
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
 
     @FXML
-    protected void singlePatient() {
-        System.out.println("single patient was pressed");
+    protected void singlePatient(ActionEvent event ) throws IOException {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(HeartDiseaseApplication.class.getResource("singlePatient.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
+            Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+            stage.setScene(scene);
+            stage.show();
+
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @FXML
