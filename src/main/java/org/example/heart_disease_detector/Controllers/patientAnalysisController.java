2bebb10580d5b9717f898499d71c07a2a0d6bb30
tbale28 <tbale28@wgu.edu>
2024-05-04 15:57:26 -0500

    PatientAnalysis Added

---
+++ src/main/java/org/example/heart_disease_detector/Controllers/patientAnalysisController.java
@@ -0,0 +1,28 @@
+package org.example.heart_disease_detector.Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.example.heart_disease_detector.HeartDiseaseApplication;
+
+import java.io.IOException;
+
+public class patientAnalysisController {
+    @FXML
+    protected void home_btn(ActionEvent event) throws IOException {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(HeartDiseaseApplication.class.getResource("main.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
+            Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+            stage.setScene(scene);
+            stage.show();
+
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
